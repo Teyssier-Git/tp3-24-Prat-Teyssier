ab54a69cb8e3efedbbd511bf353e39e3f3b395e4
cdlk <popoto38697@gmail.com>
2020-03-02 22:27:10 +0100

    Fonction get film by director and name et Podcaster

--- tp3-helpers.php
+++ tp3-helpers.php
@@ -9,7 +9,7 @@
 function tmdbget($urlcomponent, $params=null) {
     $apikey = 'ebb02613ce5a2ae58fde00f4db95a9c1';
     $apiprefix = 'http://api.themoviedb.org/3/';  //3rd API version
-	
+
 	$targeturl = $apiprefix . $urlcomponent . '?api_key=' . $apikey;
     $targeturl .= (isset($params) ? '&' . http_build_query($params) : '');
     list($content, $info) = smartcurl($targeturl);
@@ -22,7 +22,7 @@ function tmdbget($urlcomponent, $params=null) {
  * curl wrapper
  * @param string $url
  * @return string $content
- **/  
+ **/
 function smartcurl($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -34,4 +34,27 @@ function smartcurl($url) {
     $info = curl_getinfo($ch);
     curl_close($ch);
     return [$rawcontent, $info];
+}
+
+function get_film_director($film_id){
+  $castData = json_decode(tmdbget("movie/".$film_id."/credits"), TRUE);
+  $i = 0;
+  $current_cast = $castData['crew'][$i];
+  while ($current_cast['job'] != "Director" && isset($current_cast)) {
+    $i++;
+    $current_cast = $castData['crew'][$i];
+  }
+  return $current_cast;
+}
+
+function get_films_by_title_and_director($title, $director_name){
+    str_replace(' ','+',$title);
+    $data = json_decode(tmdbget("search/movie/",['query' => $title]), TRUE);
+    $films_data = array();
+    foreach ($data['results'] as $film) {
+      if(strcmp(get_film_director($film["id"])['name'],$director_name)==0){
+          array_push($films_data, $film['id']);
+      }
+    }
+    return $films_data;
 }
