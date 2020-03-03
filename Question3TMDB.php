fe98805cdbbe533478491e617175842cdff71e6d
Teyssier <theo.teyssier@etu.univ-grenoble-alpes.fr>
2020-03-03 09:39:02 +0100

    Question 4

--- Question3TMDB.php
+++ Question3TMDB.php
@@ -1,19 +1,56 @@
 <!DOCTYPE html>
 <?php
 require_once('tp3-helpers.php');
-$data = json_decode(tmdbget("movie/".$_GET['film_key'],['language' => 'fr']), TRUE);
+$data_fr = json_decode(tmdbget("movie/".$_GET['film_key'],['language' => 'fr']), TRUE);
+$data_en = json_decode(tmdbget("movie/".$_GET['film_key'],['language' => 'en']), TRUE);
+$vo = $data_en['original_language'];
+$data_vo = json_decode(tmdbget("movie/".$_GET['film_key'],['language' => "$vo"]), TRUE);
 ?>
 <html lang="en" dir="ltr">
-  <head>
+<head>
     <meta charset="utf-8">
-    <title>Film info : <?php echo $data["title"]?></title>
-  </head>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
   <body>
-    <h2>Titre du film : <?php echo $data["title"]?></h2>
-    <h4>Titre original : <?php echo $data["original_title"]?></h4>
-    <h4>Tag : <?php echo ($data["tagline"]!=NULL ? $data["tagline"] : "Aucun")?></h4>
-    <h4>Description :</h4>
-    <p><?php echo $data["overview"]?></p>
-    <h5>Retrouver plus d'information : <a href="<?php echo $data["homepage"]?>">ICI</a></h4>
+    <table>
+        <tbody>
+            <tr>
+            <th>VO</th>
+            <th>FR</th>
+            <th>EN</th>
+            </tr>
+            <tr>
+            <?php
+                $data = $data_vo;
+                echo "<td>";
+                echo "<h2>Titre du film : ",$data["title"],"</h2>";
+                echo "<h4>Titre original : ",$data["original_title"],"</h4>";
+                echo "<h4>Tag : ",($data["tagline"]!=NULL ? $data["tagline"] : "Aucun"),"</h4>";
+                echo "<h4>Description :</h4>";
+                echo "<p>",$data["overview"],"</p>";
+                echo "<h5>Retrouver plus d'information : <a href='",$data["homepage"],"'>ICI</a></h4>";
+                echo "</td>";
+                $data = $data_fr;
+                echo "<td>";
+                echo "<h2>Titre du film : ",$data["title"],"</h2>";
+                echo "<h4>Titre original : ",$data["original_title"],"</h4>";
+                echo "<h4>Tag : ",($data["tagline"]!=NULL ? $data["tagline"] : "Aucun"),"</h4>";
+                echo "<h4>Description :</h4>";
+                echo "<p>",$data["overview"],"</p>";
+                echo "<h5>Retrouver plus d'information : <a href='",$data["homepage"],"'>ICI</a></h4>";
+                echo "</td>";
+                $data = $data_en;
+                echo "<td>";
+                echo "<h2>Titre du film : ",$data["title"],"</h2>";
+                echo "<h4>Titre original : ",$data["original_title"],"</h4>";
+                echo "<h4>Tag : ",($data["tagline"]!=NULL ? $data["tagline"] : "Aucun"),"</h4>";
+                echo "<h4>Description :</h4>";
+                echo "<p>",$data["overview"],"</p>";
+                echo "<h5>Retrouver plus d'information : <a href='",$data["homepage"],"'>ICI</a></h4>";
+                echo "</td>";
+             ?>
+             </tr>
+        </tbody>
+    </table>
   </body>
 </html>
